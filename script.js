9ccb5a62f144edf6b207408d2b03070dc807a7ec
Elecktry <ezequielcostaa015@gmail.com>
2023-02-16 20:09:11 -0300

    arrumando todos os elementos do cartão

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 window.onload = function getNumberCard() {
     var formNumber = document.querySelector('.cartao')
 
+    var nameCard = document.querySelector('.name')
+
+    var cvv = document.querySelector('.cvv')
+
     var maskOptions = {
         mask: '0000 0000 0000 0000'
 
     };
 
-
-
+  
 
     var mask = IMask(formNumber, maskOptions)
 
@@ -22,6 +25,34 @@ window.onload = function getNumberCard() {
             escrNum.innerHTML = number
         
         console.log(number)
+
+    })
+
+    nameCard.addEventListener("input", () => {
+
+        var  nameCardForm = document.getElementById('floatingName').value
+
+
+        var escrName = document.getElementById('escrName')
+
+        
+            escrName.innerHTML = nameCardForm
+        
+      
+
+    })
+
+    cvv.addEventListener("input", () => {
+
+        var  cvvCard = document.getElementById('floatingcvv').value
+
+
+        var escrcvv = document.getElementById('escrcvv')
+
+        
+            escrcvv.innerHTML = cvvCard
+        
+      
 
     })
 
